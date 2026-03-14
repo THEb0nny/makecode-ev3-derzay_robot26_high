cdf7765d18357fe9d212290e79780df29d7903b1
Dmitriy Antipov <aston_bonny@mail.ru>
2026-03-14 22:56:57 +0300

    Обновить pxt.json, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -7,3 +7,23 @@ enum UnloadingMechanismState {
     Down,
     Up
 }
+
+// Манипулятор захвата
+function Manipulator(state: ManipulatorState, hold: boolean, v: number = 50) {
+    const dir = state == ManipulatorState.Down ? 1 : -1;
+    manipulatorMotor.run(Math.abs(v) * dir);
+    pause(10);
+    manipulatorMotor.pauseUntilStalled();
+    manipulatorMotor.setBrake(hold);
+    manipulatorMotor.stop();
+}
+
+// Механизм сброса
+function UnloadingMechanism(state: UnloadingMechanismState, hold: boolean, v: number = 30) {
+    const dir = state == UnloadingMechanismState.Down ? 1 : -1;
+    unloadingMechanismMotor.run(Math.abs(v) * dir);
+    pause(10);
+    unloadingMechanismMotor.pauseUntilStalled();
+    unloadingMechanismMotor.setBrake(hold);
+    unloadingMechanismMotor.stop();
+}
