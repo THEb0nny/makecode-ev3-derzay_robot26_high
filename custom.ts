5cb6b9f1f927287cb5a2048c751d589040f82f20
Dmitriy Antipov <aston_bonny@mail.ru>
2026-03-18 01:24:19 +0300

    Обновить pxt.json, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -27,3 +27,49 @@ function UnloadingMechanism(state: UnloadingMechanismState, hold: boolean, v: nu
     unloadingMechanismMotor.setBrake(hold);
     unloadingMechanismMotor.stop();
 }
+
+// Получить цвет
+function GetColor(debug: boolean = false): number {
+    const rgbHsvl = sensors.getRgbHsvl(colorSensor);
+    const color = sensors.convertHsvlToColorNum(rgbHsvl[1], sensors.getHsvlToColorNumBoundaries(colorSensor));
+    if (debug) {
+        brick.clearScreen();
+        brick.printValue("r", rgbHsvl[0][0], 1);
+        brick.printValue("g", rgbHsvl[0][1], 2);
+        brick.printValue("b", rgbHsvl[0][2], 3);
+        brick.printValue("h", rgbHsvl[1][0], 5);
+        brick.printValue("s", rgbHsvl[1][1], 6);
+        brick.printValue("v", rgbHsvl[1][2], 7);
+        brick.printValue("l", rgbHsvl[1][3], 8);
+        brick.printValue("color", color, 10);
+    }
+    return color;
+}
+
+// Проверка цвета
+function CheckColor(time: number, debug: boolean): number {
+    let colorSamples: number[] = [];
+    control.timer1.reset();
+    let prevTime = control.millis();
+    while (control.timer1.millis() < time) {
+        const currTime = control.millis();
+        const dt = currTime - prevTime;
+        prevTime = currTime;
+        const color = GetColor(debug);
+        colorSamples.push(color);
+        control.pauseUntilTime(currTime, 10);
+    }
+    const colorResult = custom.mostFrequentNumber(colorSamples);
+    return colorResult;
+}
+
+// Озвучить цвет
+function VoiceColor(color: number) {
+    if (color == 1) music.playSoundEffect(sounds.colorsBlack);
+    if (color == 2) music.playSoundEffect(sounds.colorsBlue);
+    else if (color == 3) music.playSoundEffect(sounds.colorsGreen);
+    else if (color == 4) music.playSoundEffect(sounds.colorsYellow);
+    else if (color == 5) music.playSoundEffect(sounds.colorsRed);
+    else if (color == 6) music.playSoundEffect(sounds.colorsWhite);
+    else music.playSoundEffect(sounds.communicationNo);
+}
--- main.ts
+++ main.ts
@@ -66,52 +66,6 @@ navigation.buildGraph([
 
 let colors: number[] = []; // Массив, чтобы сохранить цвета кубиков
 
-// Получить цвет
-function GetColor(debug: boolean = false): number {
-    const rgbHsvl = sensors.getRgbHsvl(colorSensor);
-    const color = sensors.convertHsvlToColorNum(rgbHsvl[1], sensors.getHsvlToColorNumBoundaries(colorSensor));
-    if (debug) {
-        brick.clearScreen();
-        brick.printValue("r", rgbHsvl[0][0], 1);
-        brick.printValue("g", rgbHsvl[0][1], 2);
-        brick.printValue("b", rgbHsvl[0][2], 3);
-        brick.printValue("h", rgbHsvl[1][0], 5);
-        brick.printValue("s", rgbHsvl[1][1], 6);
-        brick.printValue("v", rgbHsvl[1][2], 7);
-        brick.printValue("l", rgbHsvl[1][3], 8);
-        brick.printValue("color", color, 10);
-    }
-    return color;
-}
-
-// Проверка цвета
-function CheckColor(time: number, debug: boolean): number {
-    let colorSamples: number[] = [];
-    control.timer1.reset();
-    let prevTime = control.millis();
-    while (control.timer1.millis() < time) {
-        const currTime = control.millis();
-        const dt = currTime - prevTime;
-        prevTime = currTime;
-        const color = GetColor(debug);
-        colorSamples.push(color);
-        control.pauseUntilTime(currTime, 10);
-    }
-    const colorResult = custom.mostFrequentNumber(colorSamples);
-    return colorResult;
-}
-
-// Озвучить цвет
-function VoiceColor(color: number) {
-    if (color == 1) music.playSoundEffect(sounds.colorsBlack);
-    if (color == 2) music.playSoundEffect(sounds.colorsBlue);
-    else if (color == 3) music.playSoundEffect(sounds.colorsGreen);
-    else if (color == 4) music.playSoundEffect(sounds.colorsYellow);
-    else if (color == 5) music.playSoundEffect(sounds.colorsRed);
-    else if (color == 6) music.playSoundEffect(sounds.colorsWhite);
-    else music.playSoundEffect(sounds.communicationNo);
-}
-
 // Функция для одного ряда кубиков
 function CubeRow(firstCube: number, secondCube: number) {
     CubeCapture(firstCube); // Захватываем ближний кубик
@@ -143,15 +97,14 @@ function ReturnToCrossRowCubes() {
 // Главная функция решения задачи
 function Main() {
     for (let i = 0; i < 10; i++) { // Опрос датчиков, чтобы те включились
-        sensors.getNormalizedReflectionValue(LineSensor.Left);
-        sensors.getNormalizedReflectionValue(LineSensor.Right);
         colorSensor.rgbRaw();
         pause(5);
     }
+    sensors.preparationLineSensor();
     manipulatorMotor.setInverted(true); // Включить реверс мотора манипулятора
     unloadingMechanismMotor.setInverted(true); // Включить реверс мотора механизма сброса
-    Manipulator(ManipulatorState.Down, true, 40); // Предустановить манипулятор в положение раскрытия
-    UnloadingMechanism(UnloadingMechanismState.Up, true, 10); // Предустановить механизм сброса в положение закрыт
+    // Manipulator(ManipulatorState.Down, true, 40); // Предустановить манипулятор в положение раскрытия
+    // UnloadingMechanism(UnloadingMechanismState.Up, true, 10); // Предустановить механизм сброса в положение закрыт
     brick.printString("RUN", 7, 13);
     brick.buttonEnter.pauseUntil(ButtonEvent.Pressed); // Ожидание нажатие кнопки
     brick.clearScreen(); // Очистить экран
@@ -170,8 +123,9 @@ function Main() {
     navigation.setCurrentDirection(0);
     let path = navigation.algorithmDFS(0, 9);
     console.log(`path: ${path.join(', ')}`);
-    chassis.accelStartLinearDistMove(30, 80, 100, 100); // Плавный старт с стартовой зоны
-    navigation.followLineByPath(path, { moveStartV: 30, moveMaxV: 70, accelStartDist: 50, turnV: 80, Kp: 0.1, Kd: 0.5 });
+    // chassis.accelStartLinearDistMove(30, 80, 100, 100); // Плавный старт с стартовой зоны
+    navigation.followLineByPath(path, AfterLineMotion.HoldStop, { moveStartV: 30, moveMaxV: 70, turnV: 80, accelStartDist: 50, Kp: 0.1, Kd: 0.5 }, true);
+    // navigation.directionSpinTurn(0, 60, true);
     
     // for (let i = 0; i < 3; i++) {  // Движемся по линии и проходим три перекрёстка без остановки
     //     motions.lineFollowToCrossIntersection(AfterLineMotion.LineContinueRoll, { v: 80, Kp: 0.2, Kd: 0.5 });
